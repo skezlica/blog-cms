9b1c2e452d5834859fdfe310e3aacbf17f5b34c7
skezlica <stefannasic00@gmail.com>
2024-01-17 15:43:20 +0100

    Added html and css for home page

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,1 +1,18 @@
-<h1>View Home page</h1>
+<link rel="stylesheet" href="<?=ROOT?>/assets/css/home.css">
+
+<div class="navbar">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="<?=ROOT?>/signin">Signin</a></li>
+            <li><a href="<?=ROOT?>/signup">Signup</a></li>
+        </ul>
+    </div>
+<div class="container">
+    <div class="description">
+        <h1>BLOG CMS</h1>
+        <p>
+            "Let your online journey begin here! Our blog-CMS project provides you with a powerful tool for creating, editing, and sharing inspirational and informative articles. Discover the simplicity, feature richness, and adaptability of our system that will effortlessly help you express your ideas and connect with your audience. Elevate your online experience with intuitive content management and aesthetically pleasing design. You're ready for an unforgettable journey into the world of blogging – start it now with our blog-CMS solution!"
+        </p>
+    </div>
+</div>
+
