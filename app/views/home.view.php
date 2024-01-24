89c1141bc50786e24231c1651816871c202afce4
skezlica <stefannasic00@gmail.com>
2024-01-24 14:02:41 +0100

    Removed unnecessary buttons on the home page when they are not useful

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -4,8 +4,12 @@
 <div class="navbar">
         <ul>
             <li><a href="<?=ROOT?>/home">Home</a></li>
-            <li><a href="<?=ROOT?>/signin">Sign In</a></li>
-            <li><a href="<?=ROOT?>/signout">Sign Out</a></li>
+            <?php if (!isset($_SESSION['user'])): ?>
+                <li><a href="<?=ROOT?>/signin">Sign In</a></li>
+            <?php endif; ?>
+            <?php if (isset($_SESSION['user'])): ?>
+                <li><a href="<?=ROOT?>/signout">Sign Out</a></li>
+            <?php endif; ?>
         </ul>
     </div>
 <div class="container">
@@ -15,7 +19,11 @@
         <p>
             "Let your online journey begin here! Our blog-CMS project provides you with a powerful tool for creating, editing, and sharing inspirational and informative articles. Discover the simplicity, feature richness, and adaptability of our system that will effortlessly help you express your ideas and connect with your audience. Elevate your online experience with intuitive content management and aesthetically pleasing design. You're ready for an unforgettable journey into the world of blogging – start it now with our blog-CMS solution!"
         </p>
-        <a href="<?=ROOT?>/dashboard">GO TO DASHBOARD</a>
+        <?php if (isset($_SESSION['user'])): ?>
+            <a href="<?=ROOT?>/dashboard">GO TO DASHBOARD</a>
+        <?php else : ?>
+            <a href="<?=ROOT?>/dashboard">SIGN IN</a>
+        <?php endif; ?>
     </div>
 </div>
 
